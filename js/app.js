ec1c0006cbec97dfd8376ee639ae81466531f80e
Nils Kriedner <nkriedner@googlemail.com>
2024-01-08 10:42:31 +0100

    Capture the value of key inputs in the search field

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+// Run the lightbox plugin:
 baguetteBox.run(".gallery");
+
+/*****************************
+  MY SEARCH FILTER SOLUTION
+*****************************/
+
+// Target the search input
+const searchInput = document.querySelector("header input");
+
+// Listen for key events in search input
+searchInput.addEventListener("keyup", function (e) {
+    // Capture search input value (in lowercase letters)
+    let inputValue = e.target.value.toLowerCase();
+    console.log(inputValue);
+});
