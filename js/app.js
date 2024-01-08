b19399f56c1c4f13b3971ad27a770f02901eb1dd
Nils Kriedner <nkriedner@googlemail.com>
2024-01-08 11:47:10 +0100

    Complete search filter function in app.js

--- js/app.js
+++ js/app.js
@@ -8,9 +8,25 @@ baguetteBox.run(".gallery");
 // Target the search input
 const searchInput = document.querySelector("header input");
 
+// Get image width for current viewport
+// const imageWidth = document.querySelector("a").offsetWidth + "px";
+// console.log(imageWidth);
+
 // Listen for key events in search input
+
 searchInput.addEventListener("keyup", function (e) {
     // Capture search input value (in lowercase letters)
     let inputValue = e.target.value.toLowerCase();
-    console.log(inputValue);
+    // Target the data-caption texts
+    const images = document.querySelectorAll(".gallery a");
+    images.forEach((image) => {
+        if (image.getAttribute("data-caption").includes(inputValue)) {
+            // image.style.display = "inline-block";
+            image.style.display = "inline-block";
+            // image.style.width = imageWidth;
+            // imageElement.style.width = imageWidth;
+        } else {
+            image.style.display = "none";
+        }
+    });
 });
