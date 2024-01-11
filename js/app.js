7b18b65f7a7d70650fdb533fe45c83b5bcabbd0e
Nils Kriedner <nkriedner@googlemail.com>
2024-01-11 10:56:23 +0100

    Add 3 empty divs in index.html + clean up app.js

--- js/app.js
+++ js/app.js
@@ -8,12 +8,7 @@ baguetteBox.run(".gallery");
 // Target the search input
 const searchInput = document.querySelector("header input");
 
-// Get image width for current viewport
-// const imageWidth = document.querySelector("a").offsetWidth + "px";
-// console.log(imageWidth);
-
 // Listen for key events in search input
-
 searchInput.addEventListener("keyup", function (e) {
     // Capture search input value (in lowercase letters)
     let inputValue = e.target.value.toLowerCase();
@@ -21,10 +16,7 @@ searchInput.addEventListener("keyup", function (e) {
     const images = document.querySelectorAll(".gallery a");
     images.forEach((image) => {
         if (image.getAttribute("data-caption").includes(inputValue)) {
-            // image.style.display = "inline-block";
             image.style.display = "inline-block";
-            // image.style.width = imageWidth;
-            // imageElement.style.width = imageWidth;
         } else {
             image.style.display = "none";
         }
